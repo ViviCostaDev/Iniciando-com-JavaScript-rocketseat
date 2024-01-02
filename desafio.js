dadf7ab031d81797fb5625fe3a2cf24e44b0e53b
ViviCostaDev <140123505+ViviCostaDev@users.noreply.github.com>
2024-01-02 15:09:37 -0300

    Add files via upload

---
+++ desafio.js
@@ -0,0 +1,75 @@
+//Number 1
+
+let world = () => {
+    
+ // alert("HELLO WORLD!")
+}
+world()
+
+//Number 2
+let numero = 15
+let numero2 = 20
+console.log(numero + numero2)
+
+//Number 3
+let typeString = "Viviane"
+
+console.log(typeof(typeString) +`: ${typeString} não é um número`)
+
+//Number 4
+
+let notString = 200.32
+console.log(typeof(notString) +`:  não é uma string`)
+
+//Number 5
+
+let woman =  true
+
+
+console.log(typeof(woman) + ": É um booleano")
+
+//Number 6
+let num0 = 35
+let num1 = 22
+console.log(num0 - num1) 
+
+    // or
+
+function subtract(num0, num1){
+    console.log(num0 - num1)
+}
+subtract(35, 22)
+
+
+//Number 7
+
+let mult = 139
+let mult1 = 48
+ console.log(139 * 48)
+
+       
+
+
+//Number 8
+
+let div0 = 135
+let div1 = 45
+ console.log(135 / 45)
+       
+//Number 9
+let  numberOne = 20; // um número qualquer
+if (numberOne % 2 === 0) { // se o resto da divisão por 2 for zero
+  console.log("É um número par"); // o número é par
+} else { // senão
+  console.log("Ímpar"); // o número é ímpar
+}
+
+//Number 10
+
+let  numberTwo = 45; // um número qualquer
+if (numberTwo % 2 === 0) { // se o resto da divisão por 2 for zero
+  console.log("É um número par"); // o número é par
+} else { // senão
+  console.log("Ímpar"); // o número é ímpar
+}
+
